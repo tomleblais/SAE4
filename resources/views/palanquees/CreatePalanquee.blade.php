b2df8212746572d312fe8d0b993e69613df61178
lepley <lepley.gaetan@etu.unicaen.fr>
2024-03-20 16:22:28 +0100

    fixing select

--- resources/views/palanquees/CreatePalanquee.blade.php
+++ resources/views/palanquees/CreatePalanquee.blade.php
@@ -14,10 +14,14 @@
     </div>
 @endif
 
+@php
+@endphp
+
 <h1 class="w3-center w3-lime"> Nouvelle palanquée </h1>
 <form action="/api/plongees/{{$plongee}}/palanquees" method="POST" style="padding: 8px">
     @csrf
-    <x-input type="number" name="max_profondeur" text="Profondeur maximale" min="0" required />
+    <x-input type="number" name="max_profondeur" text="Profondeur maximale" min="0" max="{{ $dive->lieu->LIE_prof_max }}" required placeholder="{{ $dive->lieu->LIE_prof_max }}" />
+
     <x-input type="number" name="max_duree" text="Durée maximale de plongée" min="0" required />
     <div style="text-align: center;" class="w3-padding-16">
         <input type="submit" value="Créer" class="w3-round-large" style="padding: 16px">
