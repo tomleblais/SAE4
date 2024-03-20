b35f8cebf1105d097198bb842c9a3fa47651d727
lepley <lepley.gaetan@etu.unicaen.fr>
2024-03-20 11:43:35 +0100

    resolving conflict

--- app/Models/Adherent.php
+++ app/Models/Adherent.php
@@ -1,3 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\AutorisationsFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 /**
  * Class Adherent
  *
